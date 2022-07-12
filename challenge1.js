c9f58e9dc23885f5b97cfaa93bfb7f6f98be8e06
David Mok <davidemok@gmail.com>
2022-07-12 13:36:02 -0500

    Initial commit

---
+++ challenge1.js
@@ -0,0 +1,15 @@
+let x = 5
+let y = 3
+
+if(x > y){
+    console.log ("X is more chungus")
+}
+if(y > x){
+    console.log ("Y is more chungus")
+}
+
+if (x == y){
+    console.log ("They are the same value you dodobird")
+}
+
+
